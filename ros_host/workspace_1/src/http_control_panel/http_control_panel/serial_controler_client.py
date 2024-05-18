13809c1ea91ac30ebcb94e60321368a4ea9e6dff
serg4life <sergimendi2012@gmail.com>
2024-05-18 12:09:27 +0200

    serial_controller_client added.

---
+++ ros_host/workspace_1/src/http_control_panel/http_control_panel/serial_controler_client.py
@@ -0,0 +1,27 @@
+import rclpy
+import sys
+
+from custom_interfaces.srv import PointsToSerial
+from rclpy.node import Node
+
+class serial_controller_client(Node):
+
+    def __init__(self):
+        super().__init__('serial_controller_client')
+        self.cli = self.create_client(PointsToSerial, 'PointsToSerial')
+
+    def send_request(self, positions, times, n_points, max_connect_retries=2):
+        retries = 0
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            if retries >= max_connect_retries:
+                raise TimeoutError("Service not available, retries limit reached ({}).".format(retries))
+            self.get_logger().info("service not available, waiting again...")
+            retries += 1
+        
+        self.req.positions = positions
+        self.req.times = times
+        self.req.n_points = n_points
+        
+        self.future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
