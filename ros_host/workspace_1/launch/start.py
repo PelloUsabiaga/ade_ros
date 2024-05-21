d15b02019fd7541415090196fd441ab34cf4fdfb
serg4life <sergimendi2012@gmail.com>
2024-05-21 19:22:57 +0200

    launch file updated

--- ros_host/workspace_1/launch/start.py
+++ ros_host/workspace_1/launch/start.py
@@ -4,8 +4,18 @@ from launch_ros.actions import Node
 def generate_launch_description():
 	return LaunchDescription([
 		Node(
-			package='',
-			executable='',
-			name=''
-		)
+			package='http_control_panel',
+			executable='flask_web_Server',
+			name='http'
+		),
+		Node(
+			package='serial_controller',
+			executable='serial_controller_server',
+			name='serial'
+		),
+		Node(
+			package='trajectory_interpolation',
+			executable='trajectory_interpolation_server',
+			name='interpolation'
+		),
 	])
