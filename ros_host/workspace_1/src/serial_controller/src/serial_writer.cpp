61d7c7efb97749c38296ac9f0d7512ebc844b933
Ubuntu <pellousabiaga@gmail.com>
2024-05-23 14:31:24 +0000

    basic thread added

--- ros_host/workspace_1/src/serial_controller/src/serial_writer.cpp
+++ ros_host/workspace_1/src/serial_controller/src/serial_writer.cpp
@@ -3,13 +3,24 @@
 #include <wiringSerial.h>
 #include <iomanip>
 #include <sstream>
+#include <chrono>
 
 serial_writer::serial_writer(std::string serial_device){
     std::cout << serial_device << std::endl;
+    std::cout << "here end" << std::endl;
+
+    this->run_in_background = true;
+
+    this->background_thread = std::thread{&serial_writer::read_from_serial_in_background, this};
     if((this->fd=serialOpen(serial_device.c_str(), 115200))<0){
         throw std::logic_error("Unable to open serial device.\n");
     }
+    std::cout << "constructor end" << std::endl;
 };
+
+serial_writer::~serial_writer(){
+    this->run_in_background = false;
+}
 
 int serial_writer::write_to_serial(std::vector<double> positions, std::vector<double> times, int n_points){
     std::string buffer;
@@ -46,5 +57,14 @@ int serial_writer::write_to_serial(std::vector<double> positions, std::vector<do
     serialFlush(this->fd); 
 
     return 0;
+};
+
+
+void serial_writer::read_from_serial_in_background(){
+    std::cout << "thread start" << std::endl;
+    while (this->run_in_background){
+        std::cout << "kaixo" << std::endl;
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+    }
 };
 
--- ros_host/workspace_1/src/serial_controller/src/serial_writer.h
+++ ros_host/workspace_1/src/serial_controller/src/serial_writer.h
@@ -4,12 +4,17 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <thread>
 
 class serial_writer{
     private:
         int fd;
+        void read_from_serial_in_background();
+        bool run_in_background;
+        std::thread background_thread;
     public:
         serial_writer(std::string serial_device);
+        ~serial_writer();
         int write_to_serial(std::vector<double> positions, std::vector<double> times, int points);
 };
 #endif
