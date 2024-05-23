9c89f71270ba339faf0120f9e0f99f349293e040
Ubuntu <pellousabiaga@gmail.com>
2024-05-23 20:35:19 +0000

    It works! graphical display available

--- ros_host/workspace_1/src/http_control_panel/http_control_panel/flask_web_server.py
+++ ros_host/workspace_1/src/http_control_panel/http_control_panel/flask_web_server.py
@@ -1,8 +1,14 @@
+import io
 import os
+import random
+import time
 import flask
 import threading
 
 import rclpy
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 
 from .ros_controler_node import ros_controler_node
@@ -20,28 +26,32 @@ class http_control_panel:
 
         self.points = 3
         self.mean_speed = 0.1
+
+        self.position_list = [0]
+        self.time_list = [time.time()]
         
         self.init_ros()
 
     def init_ros(self):
         rclpy.init()
-        self.rc_node = ros_controler_node()
-        self.serial_node = serial_controller_client()
+        self.rc_node = ros_controler_node(self.position_received_callback)
         self.executor = rclpy.executors.MultiThreadedExecutor()
         self.executor.add_node(self.rc_node)
-
-    
 
         self.rc_node_spin_thread = threading.Thread(target=self.executor.spin, daemon=True)
         self.rc_node_spin_thread.start()
 
+    def position_received_callback(self, position, timestamp):
+        print("called!")
+        self.position_list.append(position)
+        self.time_list.append(time.time())
+
 
     def create_flask_app(self):
         template_dir = os.path.abspath('src/http_control_panel/http_control_panel/templates')
-        print(template_dir)
         app = flask.Flask(__name__, template_folder=template_dir)
         app.add_url_rule("/", "index", self.index_view, methods=('GET', 'POST'))
-        app.add_url_rule("/add/<number>", "/add/<number>", self.number_view)
+        app.add_url_rule('/plot.png', "image", self.plot_trajectory_png, methods=('GET', 'POST'))
         return app
 
     def index_view(self):
@@ -57,22 +67,33 @@ class http_control_panel:
             self.trajectory_times = result.times
             self.current_position = new_target_position
 
-            self.serial_node.send_write_request(result.positions, result.times, n_points=30)
+            self.rc_node.send_write_trajectory_request(result.positions, result.times, n_points=30)
 
             return flask.redirect(flask.url_for('index'))
 
+        plt.plot(self.time_list, self.position_list)
+        
         return flask.render_template('index.html', target_position=self.target_position,
                                      current_position=self.target_position, 
                                      trajectory_points=self.trajectory_points, trajectory_times=self.trajectory_times,
                                      default_points=self.points, default_mean_speed=self.mean_speed)
 
+    def create_figure(self):
+        fig = Figure()
+        axis = fig.add_subplot(1, 1, 1)
+        self.time_list.append(time.time())
+        self.position_list.append(self.position_list[-1])
+        axis.plot(self.time_list, self.position_list)
+        return fig
+    
 
-    def number_view(self, number):
-        int_number = int(number)
-
-        result = self.rc_node.send_get_trajectory_request(0, 10, 1, 7)
-        self.serial_node.send_write_request(result.positions, result.times, n_points=30)
-        return "Hello world {}, {}".format(result.positions, result.times)
+    def plot_trajectory_png(self):
+        fig = self.create_figure()
+        output = io.BytesIO()
+        FigureCanvas(fig).print_png(output)
+        return flask.Response(output.getvalue(), mimetype='image/png')
+    
+   
 
     def close(self):
         self.rc_node_spin_thread.join()
--- ros_host/workspace_1/src/http_control_panel/http_control_panel/ros_controler_node.py
+++ ros_host/workspace_1/src/http_control_panel/http_control_panel/ros_controler_node.py
@@ -1,36 +1,67 @@
 import rclpy
 
 from custom_interfaces.srv import InterpolateTrajectory
+from custom_interfaces.srv import PointsToSerial
+from custom_interfaces.msg import PositionMeasurement
+
 from rclpy.node import Node
 
 
 class ros_controler_node(Node):
-    def __init__(self):
+    def __init__(self, position_received_callback):
         super().__init__("ros_controler_in_http_panel")
-        self.cli = self.create_client(InterpolateTrajectory, "interpolate_trajectory")
+        self.position_received_callback = position_received_callback
 
-        self.req = InterpolateTrajectory.Request()
+
+        self.interpolation_client = self.create_client(InterpolateTrajectory, "interpolate_trajectory")
+        self.interpolate_request = InterpolateTrajectory.Request()
 
         self.allowed_interpolations = ("linear", "cubic")
 
+        self.points_to_serial_client = self.create_client(PointsToSerial, 'write_serial')
+        self.points_to_serial_request = PointsToSerial.Request()
+
+        self.position_subscription = self.create_subscription(
+            PositionMeasurement,
+            'motor_position',
+            self.motor_position_received_callback,
+            10)
         
+    def motor_position_received_callback(self, msg):
+        self.get_logger().info("Position received: {} at time: {}".format(msg.position, msg.timestamp))
+        self.position_received_callback(msg.position, msg.timestamp)
 
     def send_get_trajectory_request(self, current_position, target_position, mean_speed, points, interpolation_type="linear", max_connect_retries=2):
         if interpolation_type not in self.allowed_interpolations:
             raise ValueError("Interpolation type {} not supported. Use only: {}.".format(interpolation_type, self.allowed_interpolations))
         
         retries = 0
-        while not self.cli.wait_for_service(timeout_sec=1.0):
+        while not self.interpolation_client.wait_for_service(timeout_sec=1.0):
             if retries >= max_connect_retries:
                 raise TimeoutError("Service not available, retries limit reached ({}).".format(retries))
             self.get_logger().info("service not available, waiting again...")
             retries += 1
 
-        self.req.current_position = float(current_position)
-        self.req.target_position = float(target_position)
-        self.req.mean_speed = float(mean_speed)
-        self.req.points = int(points)
-        self.req.interpolation_type = str(interpolation_type)
-        self.future = self.cli.call_async(self.req)
-        rclpy.spin_until_future_complete(self, self.future)
-        return self.future.result()
+        self.interpolate_request.current_position = float(current_position)
+        self.interpolate_request.target_position = float(target_position)
+        self.interpolate_request.mean_speed = float(mean_speed)
+        self.interpolate_request.points = int(points)
+        self.interpolate_request.interpolation_type = str(interpolation_type)
+        result = self.interpolation_client.call(self.interpolate_request)
+
+        return result
+    
+    def send_write_trajectory_request(self, positions, times, n_points, max_connect_retries=2):
+        retries = 0
+        while not self.points_to_serial_client.wait_for_service(timeout_sec=1.0):
+            if retries >= max_connect_retries:
+                raise TimeoutError("Service not available, retries limit reached ({}).".format(retries))
+            self.get_logger().info("service not available, waiting again...")
+            retries += 1
+        
+        self.points_to_serial_request.positions = positions
+        self.points_to_serial_request.times = times
+        self.points_to_serial_request.n_points = float(n_points)
+        
+        result = self.points_to_serial_client.call(self.points_to_serial_request)
+        return result
